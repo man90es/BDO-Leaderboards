83b90d2435d0698992872f99ccb135a47c7993b3
man90 <man90@tuta.io>
2021-03-09 09:18:18 +0100

    Add life fame leader board

---
+++ src/components/LifeFameLeaderBoard.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="leader-board">
+		<LeaderBoardLine :familyName="'Family Name'" :score="'Life Fame'" />
+		<LeaderBoardLine v-for="p in participants" :key="p.familyName"
+			:hidePlace="p.groupWPrev"
+			:place="p.place"
+			:colour="p.colour"
+			:familyName="p.familyName"
+			:score="p.lifeFame"
+		/>
+	</div>
+</template>
+
+<script>
+	import LeaderBoardLine from './LeaderBoardLine.vue'
+	import { getNumericSpec, sortByAttribute, assignPlaces } from '../helpers'
+
+	function Participant(familyName, lifeFame) {
+		this.familyName = familyName
+		this.lifeFame = lifeFame
+		this.place = 1
+		this.colour = 0
+		this.groupWPrev = false
+	}
+
+	export default {
+		name: 'ContributionLeaderBoard',
+		props: ['guildName'],
+		components: { LeaderBoardLine },
+		computed: {
+			participants() {
+				return this.$store.getters.members(this.guildName)
+					.filter((member) => { // Filter out members with private spec levels
+						return member.characters[0].specLevels !== undefined
+					})
+					.map((member) => { // Convert members to participants
+						// https://www.blackdesertfoundry.com/family-fame-guide <- this algo is wrong
+
+						let lifeFame = member.characters
+							.reduce((fame, character) => {
+								return fame + Object.values(character.specLevels).reduce((cFame, specLevel) => {
+									let numericSpec = getNumericSpec(specLevel)
+
+									return cFame + (numericSpec > 30 ? numericSpec / 2 : 0) + (numericSpec > 80 ? numericSpec : 0)
+								}, 0)
+							}, 1)
+
+						return new Participant(member.familyName, Math.floor(lifeFame))
+					})
+					.sort(sortByAttribute('lifeFame'))
+					.map(assignPlaces('lifeFame'))
+			}
+		}
+	}
+</script>
