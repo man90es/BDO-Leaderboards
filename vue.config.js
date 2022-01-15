4da5e558afe150e935bac74538816ea2e696b6e8
man90 <man90@tuta.io>
2022-01-15 10:57:20 +0100

    Make public path configurable via the .env file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/bdo-leader-boards/'
-		: '/'
+	publicPath: process.env.NODE_ENV === "production"
+		? process.env.VUE_APP_PUBLIC_PATH || "/bdo-leaderboards/"
+		: "/"
 }
