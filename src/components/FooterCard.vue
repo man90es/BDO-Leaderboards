1e92948d792e46b162f5b35ce30426554d55bc48
man90 <man90@tuta.io>
2022-04-23 10:44:57 +0200

    Update footer links

--- src/components/FooterCard.vue
+++ src/components/FooterCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<content-card>
-		<a href="https://github.com/octoman90/BDO-Leaderboards" target="_blank" :style="{ backgroundImage: assets.star }">Give me a star on GitHub!</a>
+		<a href="https://github.com/octoman90/BDO-Leaderboards" target="_blank" :style="{ backgroundImage: assets.star }">Give BDO Leaderboards a star on GitHub!</a>
 		<a href="https://github.com/octoman90/BDO-REST-API" target="_blank" :style="{ backgroundImage: assets.construction }">Curious about the API?</a>
-		<a href="https://github.com/octoman90/BDO-Leaderboards/issues" target="_blank" :style="{ backgroundImage: assets.bug }">Found a bug?</a>
+		<a href="https://donate.hemlo.cc" target="_blank" :style="{ backgroundImage: assets.donate }">Support the service</a>
 	</content-card>
 </template>
 
@@ -10,9 +10,9 @@
 	import ContentCard from "@/components/ContentCard.vue"
 
 	const assets = {
-		star: `url(${process.env.BASE_URL}assets/star_black_24dp.svg)`,
 		construction: `url(${process.env.BASE_URL}assets/construction_black_24dp.svg)`,
-		bug: `url(${process.env.BASE_URL}assets/bug_report_black_24dp.svg)`,
+		donate: `url(${process.env.BASE_URL}assets/paid_black_24dp.svg)`,
+		star: `url(${process.env.BASE_URL}assets/star_black_24dp.svg)`,
 	}
 </script>
 
