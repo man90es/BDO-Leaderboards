ba9adae83aa407d37b20c74f2cae1e4c63eff8eb
man90 <man90@tuta.io>
2024-04-06 17:20:18 +0200

    Update env variables

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production"
-		? process.env.VUE_APP_PUBLIC_PATH || "/bdo-leaderboards/"
-		: "/"
+	publicPath: process.env.VUE_APP_PUBLIC_PATH || "/"
 }
