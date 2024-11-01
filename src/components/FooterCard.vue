219b2a318aad8bb63904d079c38e1179d2c8eb37
man90 <man90@tuta.io>
2024-11-01 11:27:09 +0100

    Update footer links

--- src/components/FooterCard.vue
+++ src/components/FooterCard.vue
@@ -1,38 +1,40 @@
 <template>
 	<div class="bs-card">
-		<a
-			:href
-			:key="href"
-			:style="{ backgroundImage: icon }"
-			class="bs-link"
-			target="_blank"
-			v-for="{ href, icon, name } of links"
-		>
+		<a :href :key="href" class="bs-link" target="_blank" v-for="{ href, icon, name } of links">
+			<component :is="icon" />
 			{{ name }}
 		</a>
 	</div>
 </template>
 
 <script lang="ts" setup>
+	import { CopyrightCircleOutlined, GithubFilled, MessageFilled, MoneyCollectFilled, ToolFilled } from "@ant-design/icons-vue"
+
 	const links = [
 		{
+			href: "https://www.hemlo.cc/finances",
+			icon: MoneyCollectFilled,
+			name: "Donate",
+		},
+		{
+			href: "https://discord.gg/VUaEmBVKjG",
+			icon: MessageFilled,
+			name: `Discord`,
+		},
+		{
 			href: "https://github.com/man90es/BDO-Leaderboards",
-			icon: `url(${process.env.BASE_URL}assets/star_black_24dp.svg)`,
+			icon: GithubFilled,
 			name: "GitHub",
 		},
 		{
 			href: "https://github.com/man90es/BDO-REST-API",
-			icon: `url(${process.env.BASE_URL}assets/construction_black_24dp.svg)`,
+			icon: ToolFilled,
 			name: "API",
 		},
 		{
-			href: "https://www.hemlo.cc/finances",
-			icon: `url(${process.env.BASE_URL}assets/paid_black_24dp.svg)`,
-			name: "Donate",
-		},
-		{
 			href: "https://github.com/man90es",
-			name: `man90 © ${(new Date()).getFullYear()}`,
+			icon: CopyrightCircleOutlined,
+			name: `man90, ${(new Date()).getFullYear()}`,
 		},
 	]
 </script>
@@ -44,10 +46,5 @@
 		color: #24272d;
 		filter: invert(1);
 		font-size: 0.9em;
-		padding-left: 1.5em;
-
-		&:last-child {
-			padding-left: 0;
-		}
 	}
 </style>
